830c1db015f463d318020710500aa5451d84e033
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-07-11 16:30:03 -0400

    ss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Todo
-  from "./components/Todo";
+import Todo from "./components/Todo";
+
+
+
+
 function App() {
   return (
     <div>
       <h1> My Todos </h1>
-      <Todo />
+      <Todo text='hello' />
+      1:30
     </div>
 
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
-export default function Todo() {
+export default function Todo(props) {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    function deleteHandler() {
+        setModalIsOpen(true)
+    }
+
+    function closeModalHandler() {
+        setModalIsOpen(false)
+    }
+
     return (
         <div className='card'>
 
-            <h2> TITLE </h2>
+            <h2> {props.text} </h2>
 
             <div className='actions'>
-                <button className='btn' >Delete</button>
+                <button className='btn' onClick={deleteHandler} >Delete</button>
             </div>
+
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+
         </div>
     )
 }
