9eebd9d4718a3196a21d7a8820ac3d882abf55b1
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-07-11 13:57:00 -0400

    ss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
+import Todo
+  from "./components/Todo";
 function App() {
   return (
     <div>
       <h1> My Todos </h1>
-      <div>
-        <h2> TITLE </h2>
-        <button >Delete</button>
-      </div>
+      <Todo />
     </div>
 
   );
